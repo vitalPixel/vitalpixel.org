d841c2a41301ae78923e6439ecb067b5a0ed0b8b
vitalPixel <19647305+vitalPixel@users.noreply.github.com>
2024-12-20 09:48:00 +0000

    updated content section to always occupy all the available vertical space after sticking

--- scripts.js
+++ scripts.js
@@ -1,19 +1,28 @@
-const scrollContainer = document.querySelector('.hero__heading');
-const scrollContent = document.querySelector('.hero__heading__scroll');
-let reverseScrollValue = 0;
+// const scrollContainer = document.querySelector('.hero__heading');
+// const scrollContent = document.querySelector('.hero__heading__scroll');
+// let reverseScrollValue = 0;
 
-function debounce(method, delay) {
-    clearTimeout(method._tId);
-    method.tId = setTimeout(function() {
-        method();
-    }, delay);
+// function debounce(method, delay) {
+//     clearTimeout(method._tId);
+//     method.tId = setTimeout(function() {
+//         method();
+//     }, delay);
+// }
+
+// function reverseScroll () {
+//     reverseScrollValue = scrollContainer.scrollTop * 0.1;
+//     scrollContent.style.transform = `translateY(${reverseScrollValue}%)`;
+// }
+
+// scrollContainer.addEventListener('wheel', function(event) {
+//     debounce(reverseScroll, 300);
+// });
+
+function updateSeparatorHeightVar() {
+    const separator = document.querySelector('.separator');
+    const separatorHeight = separator.offsetHeight;
+    document.documentElement.style.setProperty('--separator-height', `${separatorHeight}px`);
 }
 
-function reverseScroll () {
-    reverseScrollValue = scrollContainer.scrollTop * 0.1;
-    scrollContent.style.transform = `translateY(${reverseScrollValue}%)`;
-}
-
-scrollContainer.addEventListener('wheel', function(event) {
-    debounce(reverseScroll, 300);
-});
+window.addEventListener('load', updateSeparatorHeightVar);
+window.addEventListener('resize', updateSeparatorHeightVar);
